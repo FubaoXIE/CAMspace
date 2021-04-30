7e5b0fe278270d32e00e20f09f84e1b1dcf2b6f0
SeataXIE <xiefubao1994@126.com>
2021-04-30 20:34:24 +0800

    Create main.cpp

---
+++ CAMspace/main.cpp
@@ -0,0 +1,37 @@
+#include "CAMGUIHelper.h"
+
+#include "Entry.h"
+
+
+void callback() { 
+
+	ImGui::PushItemWidth(100);
+
+	Basic_entry();
+	my_entry();
+
+	ImGui::SameLine();
+	// ImGui::InputInt("source vertex", &iVertexSource);
+	ImGui::PopItemWidth();
+}
+
+int main(int argc, char** argv) {
+
+  // Options
+  polyscope::options::programName = "CAMspace";
+  polyscope::options::printPrefix = "[CAMspace] ";
+  polyscope::options::autocenterStructures = false;
+  polyscope::view::windowWidth = 1024;
+  polyscope::view::windowHeight = 1024;
+
+  // Initialize polyscope
+  polyscope::init();
+
+  // Add the callback
+  polyscope::state::userCallback = callback;
+
+  // Show the gui
+  polyscope::show();
+
+  return EXIT_SUCCESS;
+}
